020d18a3ef05d81ad88082e449da7fa386afa551
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-06-09 21:55:34 +0300

    work

--- url_shortener/url_app/views.py
+++ url_shortener/url_app/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile
+from django.contrib.auth.hashers import make_password
+from .models import UserProfile, ShortenedUrl
+import uuid
+from datetime import datetime
 
 #* ===== ACCOUNT SYSTEM ===== *#
 
@@ -83,5 +86,48 @@ def Dashboard(request):
 #* ====== URL SHORTENING &  URL STUFF ===== *#
 
 def ShortenUrl(request):
-    
+    if request.method == "POST":
+        original_url = request.POST.get("original_url")
+        custom_alias = request.POST.get("custom_alias")
+        expiry_date = request.POST.get("expiry_date")
+        active = request.POST.get("active")
+        max_uses = request.POST.get("max_uses")
+        url_password = request.POST.get("url_password")
+        
+        hashed_url_password = make_password(url_password)
+        short_code = str(uuid.uuid4())[:20]
+
+        ShortenedUrl.objects.create(
+            original_url = original_url,
+            short_code = short_code,
+            created_at = datetime.now,
+            url_password = hashed_url_password,
+            is_active = active
+        )
+
+
+        """
+        def add(request):
+        if request.method == "POST":
+            link = request.POST['link']
+            link_id = str(uuid.uuid4())[:5]
+            new_link = LinkInfo(link=link, link_id=link_id)
+            new_link.save()
+
+        return HttpResponse(link_id)
+        """
+
     return render(request, 'app/url_create.html')
+
+"""
+    original_url = models.URLField(max_length=2048)
+    short_code = models.CharField(max_length=20, unique=True, )
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    custom_code = models.CharField(max_length=20, unique=True, null=True, blank=False)
+    expiry_date = models.DateTimeField()
+    max_uses = models.IntegerField(null=True, blank=True)
+    current_uses = models.IntegerField(default=0)
+    url_password  = models.CharField(max_length=128, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+"""
